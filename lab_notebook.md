45496afd8bd6e69bbcc90accca59f053d0038863
Nelson Krisanto <121865040+minezsz@users.noreply.github.com>
2024-02-09 21:23:53 +0000

    updated scripts

--- lab_notebook.md
+++ lab_notebook.md
@@ -1,6 +1,18 @@
 # Title
-## High level guide on how to use the scripts
-### 1. x...
+## High Level Guide to Using the Scripts
+
+### Configuration: `00.config.sh`
+Please update the script with your specific settings where you see the comment "## adjust". Ensure these settings align with your project requirements.
+
+### Fetching NCBI Data: `01.fetch_NCBI.sh`
+Modify the sections marked as "## adjust" to suit your research needs. When you reach the portion of the script that says, "# Perform the search to get dengue virus sequences. Adjust the search term as needed," refer to the instructions provided below.
+
+To tailor the search query for dengue virus sequences, use the NCBI Nucleotide Advanced Search Builder. The image below (Figure 1.1) illustrates the Advanced Search Builder interface, which you can access at the following URL: [NCBI Nucleotide Advanced Search Builder](https://www.ncbi.nlm.nih.gov/nuccore/advanced).
+
+![Figure 1.1 - NCBI Nucleotide Advanced Search Builder](images/01_01.png)
+
+In the Advanced Search Builder, adjust the search parameters as necessary. Your configured parameters will reflect in the search details field, as highlighted in Figure 1.1.
+
 
 ## Pre-requisite/Dependencies
 The following module, environment, and packages are needed in this project:
--- scripts/01.fetch_NCBI.sh
+++ scripts/01.fetch_NCBI.sh
@@ -20,15 +20,15 @@ output_dir="$my_dir/$project_dir/raw_data/NCBI_dengue_data" ## adjust
 mkdir -p "$output_dir"
 
 # Perform the search to get dengue virus sequences. Adjust the search term as needed.
-esearch -db nuccore -query "Dengue Virus[ORGN] AND 300:11000[SLEN]" | efetch -format fasta > "$output_dir/dengue_virus_sequences.fasta"
+esearch -db nuccore -query "Dengue Virus[ORGN] AND 300:11000[SLEN]" | efetch -format fasta > "$output_dir/dengue_virus_sequences.fasta" ## adjust
 
-echo "Fetched dengue virus sequences are saved in: $output_dir/dengue_virus_sequences.fasta"
+echo "Fetched dengue virus sequences are saved in: $output_dir/dengue_virus_sequences.fasta" ## adjust
 
 # Specify the input FASTA file path for dengue virus
-fasta_file="$output_dir/dengue_virus_sequences.fasta"
+fasta_file="$output_dir/dengue_virus_sequences.fasta" ## adjust
 
 # Specify the output TSV file path for dengue virus sequence information
-tsv_file="$output_dir/dengue_sequence_info.tsv"
+tsv_file="$output_dir/dengue_sequence_info.tsv" ## adjust
 
 # Rename fasta headers with accession IDs only
 sed -i 's/^>\([^ ]*\) .*/>\1/' "$fasta_file"
@@ -37,7 +37,7 @@ sed -i 's/^>\([^ ]*\) .*/>\1/' "$fasta_file"
 echo -e "accession_id\tsequence_length" > "$tsv_file"
 awk -F '[>\t]' '/^>/ {if (acc != "") print acc "\t" len; acc=$2; len=0; next} {len+=length($0)} END {if (acc != "") print acc "\t" len}' "$fasta_file" >> "$tsv_file"
 
-echo "Dengue virus sequence information is stored in: $tsv_file"
+echo "Dengue virus sequence information is stored in: $tsv_file" ## adjust
 
 # Count the number of dengue virus sequences
 num_sequences=$(grep -c '^>' "$fasta_file")
